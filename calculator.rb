0c866d8c565997dece16612c28dbb6f6d47d1af8
Angel <angelmmed12@gmail.com>
2018-03-15 11:36:49 -0500

    added ability for multiple arguments

--- calculator.rb
+++ calculator.rb
@@ -1,22 +1,34 @@
 class Calculator
 
-    def Calculator.add(num_1,num_2)
-        num_1 + num_2
+    def Calculator.add(*nums)
+      result=0
+        nums.each do |number|
+          result+=number
+        end
+        result
     end   
 
-    def Calculator.sub(num_1,num_2)
-        num_1 - num_2
+    def Calculator.sub(*nums)
+        result=nums.first
+        nums.drop(1).each do |number|
+          result-=number
+        end
+        result
     end
 
-    def Calculator.mult(num_1,num_2)
-        num_1 * num_2
+    def Calculator.mult(*nums)
+        result=nums.first
+        nums.drop(1).each do |number|
+            result*=number
+        end
+        result
     end
 
     def Calculator.div(num_1,num_2)
-        if(num_1 == 0)
-            puts "Division by 0 Error"
+        if num_2 == 0
+          return nil
         else
-            num_1/num_2
+            num_1 / num_2
         end
     end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -8,12 +8,12 @@ describe Calculator do
     end
 
     describe "::sub" do
-        it "should return 0 to the 7-7 substraction"
+        it "should return 0 to the 7-7 substraction" do
             expect(Calculator.sub(7,7)).to eq 0
         end
 
-        it "should return a negative number"
-            expect(Calculator.sub(7-10)).to eq 3
+        it "should return a negative number" do
+            expect(Calculator.sub(7,10)).to eq (-3)
         end
     end
 
@@ -28,8 +28,8 @@ describe Calculator do
             expect(Calculator.div(5,5)).to eq 1
         end
 
-        it "should return the string 'Division by 0 Error'" do
-            expect(Calculator.div(5,0)).to output("Division by 0 Error").to_stdout
+        it "should return nil" do
+            expect(Calculator.div(5,0)).to be_nil
         end
     end
 end
